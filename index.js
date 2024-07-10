114b6ee3979e525155f1b4d9c2182aba3edb74e8
Samuel Charbit <76777361+samrub06@users.noreply.github.com>
2024-07-10 15:36:19 +0300

    ok

--- index.js
+++ index.js
@@ -5,7 +5,5 @@ const PORT = process.env.PORT || 5001
 
 express()
   .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
+  .get('/', (req, res) => res.render('index.html'))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
